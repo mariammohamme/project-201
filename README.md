26fd5a8553165e568aef9db70bb8b73447f49a52
mariammohamme <mariam.mohammed942@hotmail.com>
2021-01-04 17:01:15 +0200

    lab_02

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # project-201
 the first project for customers
+LAB - 102-2
+I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+
+Author: mariam
+
---
+++ js/app.js
@@ -0,0 +1,65 @@
+'use strict';
+var userHow = prompt('Do you know who I am?');
+if(userHow.toLocaleLowerCase() == 'y' || userHow.toLocaleUpperCase()== 'YES')
+{
+    alert('this is Expected')
+} else if (userHow.toLowerCase() == 'n'|| userHow.toLocaleUpperCase() == 'NO'){
+alert('lets try to know us');
+}else  
+{
+//console.log('please enter some answer like y or YES');
+alert ("please enter some answer like y or YES");
+}
+
+
+var userTwinz = prompt('I have twins, how many twins do 1 ?!?');
+if(userTwinz.toLocaleLowerCase() == 'y' || userTwinz.toLocaleUpperCase()== 'YES')
+{
+    alert('YES I HAVE JUST ONE TWINNS')
+} else if (userTwinz.toLowerCase() == 'n'|| userTwinz.toLocaleUpperCase() == 'NO'){
+alert('BELIVE ME JUST ONE');
+}else{
+//console.log('please enter some answer like y or YES');
+alert ("please enter some answer like y or YES");
+}
+
+var userShool = prompt('I studied in 10 schools Do you believe me?!?');
+if(userShool.toLocaleLowerCase() == 'y' || userShool.toLocaleUpperCase()== 'YES')
+{
+    alert('thx this is true ')
+} else if (userShool.toLowerCase() == 'n'|| userShool.toLocaleUpperCase() == 'NO'){
+alert('why dont belive that');
+}else{
+ //console.log('please enter some answer like y or YES');
+alert ("please enter some answer like y or YES");
+}
+
+var userAnimal = prompt('I have a phobia of all animals, unfortunately, do you have this phobia like me??');
+if(userAnimal.toLocaleLowerCase() == 'y' || userAnimal.toLocaleUpperCase()== 'YES')
+{
+    alert('this is big problem i can feel u ')
+} else if (userAnimal.toLowerCase() == 'n'|| userAnimal.toLocaleUpperCase() == 'NO'){
+alert('you are vary lucky belive me');
+}
+else{
+//console.log('please enter some answer like y or YES');
+alert ("please enter some answer like y or YES");
+}
+
+
+
+var userGust = prompt('now i know how are u??');
+if(userGust.toLocaleLowerCase() == 'y' || userGust.toLocaleUpperCase()== 'YES')
+{
+    alert('wondreful ')
+} else if (userGust.toLowerCase() == 'n'|| userGust.toLocaleUpperCase() == 'NO'){
+alert('i hope to know you soon');
+}
+else{
+//console.log('please enter some answer like y or YES');
+alert ("please enter some answer like y or YES");
+}
+var userName = prompt('what is your name??');
+alert ('welcome    ' +'' + userName);
+
+
